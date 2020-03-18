ffebb040655a9ef58d883cb5ca20ccc44203b5e5
aledi99 <alediazuni@gmail.com>
2020-03-18 01:21:10 +0100

    Preparativos para el trabajo de mañana

---
+++ app/src/main/java/com/salesianostriana/dam/commons/Constants.java
@@ -0,0 +1,6 @@
+package com.salesianostriana.dam.commons;
+
+public class Constants {
+    private final String API_URL = "http://10.0.2.2:9000";
+    public static final String APP_SETTINGS_FILE = "APP_SETTINGS";
+}
